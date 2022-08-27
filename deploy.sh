9f438f5e070fb36db17ea840b7e4b54ecbf21c12
yarramrama <yarram.rama32@gmail.com>
2022-08-26 22:58:18 -0800

    newly updated...

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
+<<<<<<< HEAD
 i am in masterbranch ....
-=======
+this is modified for stash
+>>>>>>> 519184b (updated)
 hi i am in developers branch
 this is shell scripting information for deploying
 modified for deploying
